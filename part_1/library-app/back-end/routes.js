336e41190117f07ab0eff1c01ed854ce6da86498
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2021-01-01 22:29:36 +0200

    solve_Q3_part_1

--- part_1/library-app/back-end/routes.js
+++ part_1/library-app/back-end/routes.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql12');
 const db = require('./db')
+
 mainRouter.post('/books', (req, res) => {
     const { title, author, pages, publisher } = req.body;
-    const query = `INSERT INTO users (title, author, pages, publisher,published_at) VALUES (?,?,?,?,?)`
+    const query = `INSERT INTO books (title, author, pages, publisher,published_at) VALUES (?,?,?,?,?)`
     const data = [title, author, pages, publisher, new Date];
     db.query(query, data, (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    })
+})
+
+mainRouter.get('/books', (req, res) => {
+    const query = `SELECT * FROM books`
+    db.query(query,(err, result) => {
         if (err) throw err;
         res.json(result);
     })
