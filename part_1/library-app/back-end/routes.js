b1f39b8a516b80df65073124c646b3cbedda3f8a
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2021-01-01 22:32:54 +0200

    solve_Q4_part_1

--- part_1/library-app/back-end/routes.js
+++ part_1/library-app/back-end/routes.js
@@ -3,19 +3,46 @@ const db = require('./db')
 
 mainRouter.post('/books', (req, res) => {
     const { title, author, pages, publisher } = req.body;
-    const query = `INSERT INTO books (title, author, pages, publisher,published_at) VALUES (?,?,?,?,?)`
+    const query = `INSERT INTO books (title, author, pages, publisher,published_at) VALUES (?,?,?,?,?)`;
     const data = [title, author, pages, publisher, new Date];
+    db.query(query, data, (err, result) => {
+        if (err) throw err;
+        res.json("Succesfully added new book");
+    })
+})
+
+mainRouter.get('/books', (req, res) => {
+    const query = `SELECT * FROM books`;
+    db.query(query, (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    })
+})
+
+mainRouter.post('/books/:book_id', (req, res) => {
+    const { book_id } = req.params;
+    const query = `SELECT * FROM books WHERE id = ?`;
+    const data = book_id;
     db.query(query, data, (err, result) => {
         if (err) throw err;
         res.json(result);
     })
 })
 
-mainRouter.get('/books', (req, res) => {
-    const query = `SELECT * FROM books`
-    db.query(query,(err, result) => {
-        if (err) throw err;
-        res.json(result);
-    })
-})
 
+/*
+Q4) Create an API `/books/:book_id` to get a specific book from the database.
+
+Q5) Create an API `/books/:book_id` to delete a specific book from the database
+
+Q6) Add a frontend route `/library/create_book` to render the inputs that correspond to the book's attributes and a `submit` button (without functionality).
+
+Q7) Add a frontend route `/library` that will render all the books (title and author) from the database.
+
+Q8) Add a frontend route `/library/:book_id` that will render a specific book details from the database.
+
+Q9) When the `submit` button is clicked (from Q6) it should add a new book in the database, by sending a request to the correct endpoint in the backend.
+
+Q10) When rendering the book list (from Q7) add a delete button next to each book, that when clicked will delete the corresponding book from the database and the state.
+
+*/
